0884e7704337065f2768b1b5a4f55f9c4294cda5
superjoefly <superjoefly@gmail.com>
2018-07-23 01:29:12 -0600

    Extracted and refactored code;

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,11 +1,12 @@
-// Set up api key (instructions): http://links.mead.io/api-fix
+// Set up api key for project (instructions): http://links.mead.io/api-fix
 // my api key: AIzaSyD85ttCFuSbg_2hdVHvA-urD2awIOMFhsI
 
-// npm install --save request:
-const request = require('request');
 
 // npm install --save yargs
 const yargs = require('yargs');
+
+// require geocode:
+const geocode = require('./geocode/geocode');
 
 // Convert argv to yargs object; define options:
 const argv = yargs
@@ -21,38 +22,13 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-  // Encode uri:
-  var encAddress = encodeURIComponent(argv.address);
 
-
-// request takes an object and a callback:
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encAddress}&key=AIzaSyD85ttCFuSbg_2hdVHvA-urD2awIOMFhsI`,
-    json: true // Convert to javaScript object
-  },
-  // Callback:
-  (error, response, body) => {
-  // Display formatted address:
-  console.log(`Formatted Address: ${body.results[0].formatted_address}`);
-  // Display latitude and longitude:
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-
-});
-
-
-
-
-
-
-// For Reference (Viewing the Response):
-// // request takes an object and a callback:
-// request({
-//   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyD85ttCFuSbg_2hdVHvA-urD2awIOMFhsI',
-//   json: true // convert to javaScript object
-// }, (error, response, body) => {
-//   console.log("error:", error);
-//   console.log("statusCode:", response && response.statusCode);
-//   // 'Pretty Print' the object for viewing in console; indent 2 spaces:
-//   console.log("body:", JSON.stringify(body, undefined, 2));
-// });
+  // Get address / coordinates:
+  geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    // What does the callback do?
+    if (errorMessage) {
+      console.log(errorMessage);
+    } else {
+      console.log(JSON.stringify(results, undefined, 2));
+    }
+  });
---
+++ weather_app/geocode/geocode.js
@@ -0,0 +1,54 @@
+// npm install --save request:
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+  // Encode uri:
+  var encAddress = encodeURIComponent(address);
+
+  // request() takes an object and a callback:
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encAddress}&key=AIzaSyD85ttCFuSbg_2hdVHvA-urD2awIOMFhsI`,
+    json: true // Convert to javaScript object
+  },
+  // Callback:
+  (error, response, body) => {
+    // Error handling:
+    if (error) {
+      callback("Error: Unable to connect to Google serves!");
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback("Invalid address!");
+    } else if (body.status === 'OK') {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+    }
+  });
+}
+
+module.exports = {
+  geocodeAddress
+}
+
+
+
+
+
+
+
+
+
+
+
+// For Reference (Viewing the Response):
+// // request takes an object and a callback:
+// request({
+//   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyD85ttCFuSbg_2hdVHvA-urD2awIOMFhsI',
+//   json: true // convert to javaScript object
+// }, (error, response, body) => {
+//   console.log("error:", error);
+//   console.log("statusCode:", response && response.statusCode);
+//   // 'Pretty Print' the object for viewing in console; indent 2 spaces:
+//   console.log("body:", JSON.stringify(body, undefined, 2));
+// });
