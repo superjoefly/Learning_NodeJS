465442f59ac9db3a4813d7ff8f61903c62770130
superjoefly <superjoefly@gmail.com>
2018-08-05 00:39:57 -0600

    Added expect module and examples of using expect for assertions;

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -4,7 +4,16 @@ var add = (a, b) => a + b;
 
 var square = (a) => a * a;
 
+var setName = (user, fullName) => {
+  var names = fullName.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+
+  return user;
+};
+
 module.exports = {
   add,
-  square
+  square,
+  setName
 };
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,20 +3,145 @@
 // Require utils library:
 const utils = require('./utils');
 
-// Test if add function returns correct value:
-it('should add two numbers', () => {
-  var res = utils.add(2, 3);
-  // If the test fails:
-  if (res !== 5) {
-    throw new Error(`Expected 5, but got ${res}`);
+// Require expect:
+const expect = require('expect');
+
+// Custom matcher:
+expect.extend({
+  toBeDivisibleBy(received, argument) {
+    const pass = received % argument === 0;
+    if (pass) {
+      return {
+        message: () =>
+        `expected ${received} to not be divisible by ${argument}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be divisible by ${argument}`,
+          pass: false,
+      };
+    }
   }
 });
 
+
+
+// Test if add function returns correct value:
+it('should add two numbers', () => {
+
+  var res = utils.add(2, 3);
+
+  // Expect 5 to be 5 -> pass
+  expect(res).toBe(5);
+
+  // // Throw error manually:
+  // if (res !== 5) {
+  //   throw new Error(`Expected 5, but got ${res}`);
+  // }
+});
+
+
+
 // Test if square function returns correct value:
 it('should square a number', () => {
+
   var res = utils.square(3);
-  // If the test fails:
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}`);
-  }
+
+  // Expect 9 to be 9 -> pass
+  expect(res).toBe(9);
+
+  // // Throw error manually:
+  // if (res !== 9) {
+  //   throw new Error(`Expected 9, but got ${res}`);
+  // }
 });
+
+
+
+// Test for typeof:
+it('should be a number', () => {
+
+  var res = utils.add(2, 3);
+
+  // Expect typof 9 to be a number -> pass
+  expect(typeof res).toBe('number');
+});
+
+
+
+// Test for equality (value):
+it('value should be equal', () => {
+  // Expect value to equal value:
+  expect(12).toBe(12);
+
+  // Expect value not to equal value -> pass
+  expect(12).not.toBe(13);
+});
+
+
+
+// Test for equality (objects and arrays):
+it('array or object should be equal', () => {
+  // Object (equal to):
+  expect({name: 'Joey'}).toEqual({name: 'Joey'});
+
+  // Array(not equal to):
+  expect(['red', 'blue']).not.toEqual(['red', 'yellow']);
+});
+
+
+
+// Test for inclusion:
+it('should include', () => {
+  // Object
+  expect({name: 'Joey'}).toEqual(
+    expect.objectContaining({name: 'Joey'})
+  )
+
+  expect(['red', 'blue']).toEqual(
+    expect.arrayContaining(['red'])
+  )
+})
+
+
+
+// Test for divisibility:
+it('should be divisible by', () => {
+
+  var res = utils.square(3); // returns 9
+
+  // Expect 9 to be divisible by 3 -> pass
+  expect(res).toBeDivisibleBy(3);
+
+  // Expect 9 to NOT be divisible by 2 -> pass
+  expect(res).not.toBeDivisibleBy(2);
+});
+
+
+
+//////////////////////////
+
+
+
+// CHALLENGE: Check if first and last names are set:
+it('should have first and last names set', () => {
+
+  // Define user object:
+  var userObj = {
+    location: 'Boulder',
+    age: 38
+  }
+
+  // Set first and last name properties of user object:
+  var userObj = utils.setName(userObj, 'Joseph Atwood');
+
+  // Assert that userObj variable holds and object:
+  expect(typeof userObj).toBe('object');
+
+  // Check that userObj has proper first and last name values:
+  expect(userObj).toEqual(
+    expect.objectContaining({firstName: 'Joseph', lastName: 'Atwood'})
+  );
+})
